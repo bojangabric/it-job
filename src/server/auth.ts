a8762e5fc8eefee268759b6b568b4f27faad309e
Bojan Gabric <bojan@bojangabric.com>
2023-03-10 21:35:48 +0100

    feat: set up email and password login

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,10 +4,9 @@ import {
   type NextAuthOptions,
   type DefaultSession
 } from 'next-auth';
-import DiscordProvider from 'next-auth/providers/discord';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { env } from 'env.mjs';
 import { prisma } from 'server/db';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -36,30 +35,50 @@ declare module 'next-auth' {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+    maxAge: 3000
+  },
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
       }
+      return token;
+    },
+    session({ session }) {
       return session;
     }
   },
+  jwt: {
+    secret: 'super-secret',
+    maxAge: 15 * 24 * 30 * 60
+  },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials?.email
+          }
+        });
+
+        if (!user) return null;
+
+        const verifyPassword = user.password === credentials?.password;
+
+        if (!verifyPassword) return null;
+
+        return user;
+      }
     })
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ]
 };
 
