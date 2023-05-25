c71f213f9df13769d3b433e9269708b0ce412514
Bojan Gabric <bojan@bojangabric.com>
2023-05-25 21:31:11 +0200

    feat: update profile page for employer

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -10,6 +10,39 @@ const Profile = () => {
 
   if (!user) return <></>;
 
+  if (user.role === 'KANDIDAT') {
+    return (
+      <div className="mx-auto max-w-3xl py-10">
+        <div className="py-5">
+          <h3 className="text-lg font-medium leading-6 text-gray-900">
+            Vaše informacije
+          </h3>
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">
+            Ove informacije vide ljudi koji su postavili oglas na koji ste se
+            prijavili.
+          </p>
+        </div>
+        <Image image={user.image} />
+        <FieldRow fieldName="Ime i prezime">{user.name}</FieldRow>
+        <FieldRow fieldName="Email">{user.email}</FieldRow>
+        <Resume resume={user.resume} />
+      </div>
+    );
+  }
+
+  if (user.role === 'POSLODAVAC') {
+    return (
+      <div className="mx-auto max-w-3xl py-10">
+        <h3 className="py-5 text-lg font-medium leading-6 text-gray-900">
+          Vaše informacije
+        </h3>
+        <Image image={user.image} />
+        <FieldRow fieldName="Ime kompanije">{user.name}</FieldRow>
+        <FieldRow fieldName="Email">{user.email}</FieldRow>
+      </div>
+    );
+  }
+
   return (
     <div className="mx-auto max-w-3xl py-10">
       <div className="py-5">
