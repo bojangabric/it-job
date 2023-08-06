9066a34871f4f734d4ce247f8f8b131564d167f2
Bojan Gabric <bojan@bojangabric.com>
2023-08-06 11:36:10 +0200

    feat: add loading animation

--- src/components/jobs-list.tsx
+++ src/components/jobs-list.tsx
@@ -7,7 +7,21 @@ export const JobsList = () => {
   const { data: jobPosts, status } =
     api.jobPosts.getAll.useQuery(activeEnumFilters);
 
-  if (status === 'loading') return <></>;
+  if (status === 'loading')
+    return (
+      <div className="flex w-full items-center justify-center">
+        <svg className="spinner" viewBox="0 0 50 50">
+          <circle
+            className="path"
+            cx="25"
+            cy="25"
+            r="20"
+            fill="none"
+            stroke-width="5"
+          ></circle>
+        </svg>
+      </div>
+    );
 
   return (
     <div className="grid w-full grid-cols-3 gap-10 self-start">
